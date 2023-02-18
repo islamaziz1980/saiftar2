3c55718735b97e66b1722dbd0054120558ecaa56
islamaziz1980 <111305368+islamaziz1980@users.noreply.github.com>
2023-02-18 22:14:46 +0200

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,26 @@
+     function one(){
+             document.getElementById('myImage')
+             .src="img/1cart.svg";
+             document.getElementById('message')
+             .innerHTML="Hii! GeeksforGeeks people";
+         }
+
+         function seven(){
+             document.getElementById('myImage')
+             .src="img/7cart.svg";
+             document.getElementById('message')
+             .innerHTML="Bye! GeeksforGeeks people";
+         }
+         function fifteen(){
+             document.getElementById('myImage')
+             .src="img/15cart.svg";
+             document.getElementById('message')
+             .innerHTML="Bye! GeeksforGeeks people";
+         }
+         function thirty(){
+             document.getElementById('myImage')
+             .src="img/30cart.svg";
+             document.getElementById('message')
+             .innerHTML="Bye! GeeksforGeeks people";
+         }
+      
